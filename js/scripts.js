66fb344c2a25450d5efa8f1c33c6b5e8264870c7
Katlin Anderson and Mathew Akre <katlin@>
2019-03-26 15:38:29 -0700

    add js .find(p).toggle(); to allow clicking on the boxes to toggle the prompt

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@ $("document").ready(function(){
 
 
   $(".col-lg-3").click(function() {
-    $(this).next("p").toggle();
+    // $(this).next("p").toggle();
+    $(this).find('p').toggle();
   });
 
 
